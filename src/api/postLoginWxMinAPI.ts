2d64f39b97301cb612d0bc8c2bb36025cea11c21
lyc <1343437528@qq.com>
2023-11-18 08:23:41 +0800

    会员信息

---
+++ src/api/postLoginWxMinAPI.ts
@@ -0,0 +1,13 @@
+import { http } from './login'
+
+/**
+ * 小程序登录
+ * @param data 请求参数
+ */
+export const postLoginWxMinAPI = (data: LoginParams) => {
+  return http<LoginResult>({
+    method: 'POST',
+    url: '/user/login/wxMin',
+    data
+  })
+}
